f17b9f5e00f5862d336bd1fce11b81a05c11c809
hugosenari <hugosenari@gmail.com>
2023-02-07 01:16:53 -0300

    chore: indentation

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
-  description = "Dev Environment";
+  description = "An OpenAPI specification for a Nix HTTP Binary Cache";
 
-  inputs.nixpkgs.url  = "github:nixos/nixpkgs/22.05";
-  inputs.dsf.url      = "github:cruel-intentions/devshell-files";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/22.11";
+  inputs.dsf.url     = "github:cruel-intentions/devshell-files";
   inputs.dsf.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = inputs: inputs.dsf.lib.shell inputs [
-    "hello"        # import nix package
-    ./project.nix  # import nix module
-  ];
+  outputs = inputs: inputs.dsf.lib.shell inputs [ ./project.nix ];
 }
--- project.nix
+++ project.nix
@@ -1,13 +1,13 @@
 { 
   files.yaml."/api.yaml" = import ./api.nix;
-  files.direnv.enable = true;
+  # if you don't have direnv run: `nix develop --build`
+  files.direnv.enable    = true;
   # create .gitignore
   files.gitignore.enable = true;
-  files.gitignore.pattern.".direnv" = true;
-  # copy contents from https://github.com/github/gitignore
-  # to our .gitignore
+  files.gitignore.pattern.".direnv"          = true;
   files.gitignore.template."Global/Archives" = true;
   files.gitignore.template."Global/Backup"   = true;
   files.gitignore.template."Global/Diff"     = true;
-
+  # copy contents from https://github.com/github/gitignore
+  # to our .gitignore
 }
